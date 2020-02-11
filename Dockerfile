2fdd1ebb625c139cde18794d8841e2ec32901ac1
Artur Vorobiov <oleonortt@gmail.com>
2020-02-11 21:38:51 +0200

    server deployed to heroku

--- Dockerfile
+++ Dockerfile
@@ -1,21 +1,14 @@
 # base image
 FROM node:12.14.1
 
-# set working directory
-WORKDIR /app
-
-RUN npm install nodemon -g
-
-# install and cache app dependencies
-COPY package.json /app
-
-RUN npm install --silent
-
-EXPOSE 5000
+COPY package.json .
+RUN npm install
+COPY . .
 
 # start server in dev
-CMD ["nodemon", "app.js"]
+CMD ["node", "app.js"]
 
-# to run docker after build
-# docker build -t server:dev .
-# docker run -v /${PWD}:/app -v /app/node_modules -p 5000:5000 -e CHOKIDAR_USEPOLLING=true --rm server:dev
+# heroku container:login
+# heroku container:push web
+# heroku container:release web
+# heroku open
--- app.js
+++ app.js
@@ -32,23 +32,17 @@ const server  = app.listen(process.env.PORT || port, () => (
   console.log(`Server started on port ${port}`
 )));
 
-if (process.env.NODE_ENV === 'production') {
-  // Express will serve up production assets
-  app.use(express.static('client/build'));
-
-  // Express will serve up the index.html file
-  // if it doesn't recognize the route (needed for client side routing)
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
-}
-
 // init socket io
 const io = socketio(server);
 
 // Using routes
 app.use('/io', ioRouter(io));
 app.use('/api/user', usersRouter);
+
+// TODO: remove after test is done
+app.use('/heroku/test', (req, res) => {
+  res.send('all good');
+});
 
 // config passport to use jwtStrategy
 jwtStrategy(passport);
